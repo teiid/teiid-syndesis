8fae1318df7c14b00b2a6f70e3a41cc6e113639b
shawkins <shawkins@redhat.com>
2019-07-08 15:01:31 -0400

    removing invalid view assumptions

--- server/komodo-relational/src/main/java/org/komodo/relational/model/internal/ViewImpl.java
+++ server/komodo-relational/src/main/java/org/komodo/relational/model/internal/ViewImpl.java
@@ -17,11 +17,7 @@
  */
 package org.komodo.relational.model.internal;
 
-import org.komodo.relational.model.ForeignKey;
 import org.komodo.relational.model.Model;
-import org.komodo.relational.model.PrimaryKey;
-import org.komodo.relational.model.Table;
-import org.komodo.relational.model.UniqueConstraint;
 import org.komodo.relational.model.View;
 import org.komodo.spi.KException;
 import org.komodo.spi.repository.KomodoObject;
@@ -70,69 +66,6 @@ public final class ViewImpl extends TableImpl implements View {
         final KomodoObject parent = super.getParent( transaction );
         final Model result = Model.RESOLVER.resolve( transaction, parent );
         return result;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.komodo.relational.model.internal.TableImpl#addForeignKey(org.komodo.spi.repository.Repository.UnitOfWork,
-     *      java.lang.String, org.komodo.relational.model.Table)
-     * @throws UnsupportedOperationException
-     *         if called
-     */
-    @Override
-    public ForeignKey addForeignKey( final UnitOfWork uow,
-                                     final String foreignKeyName,
-                                     final Table referencedTable ) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.komodo.relational.model.View#addUniqueConstraint(org.komodo.spi.repository.Repository.UnitOfWork,
-     *      java.lang.String)
-     * @throws UnsupportedOperationException
-     *         if called
-     */
-    @Override
-    public UniqueConstraint addUniqueConstraint( final UnitOfWork transaction,
-                                                 final String constraintName ) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.komodo.relational.model.View#removeForeignKey(org.komodo.spi.repository.Repository.UnitOfWork, java.lang.String)
-     */
-    @Override
-    public void removeForeignKey( final UnitOfWork transaction,
-                                  final String foreignKeyToRemove ) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.komodo.relational.model.View#removeUniqueConstraint(org.komodo.spi.repository.Repository.UnitOfWork,
-     *      java.lang.String)
-     */
-    @Override
-    public void removeUniqueConstraint( final UnitOfWork transaction,
-                                        final String constraintToRemove ) {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @see org.komodo.relational.model.View#setPrimaryKey(org.komodo.spi.repository.Repository.UnitOfWork, java.lang.String)
-     */
-    @Override
-    public PrimaryKey setPrimaryKey( final UnitOfWork transaction,
-                                     final String newPrimaryKeyName ) {
-        throw new UnsupportedOperationException();
     }
 
 }
